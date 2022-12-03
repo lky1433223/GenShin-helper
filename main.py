a23e9476c90e3930dd7d2041d29e2938021f11c9
klccc <314944188@qq.com>
2022-12-03 16:24:18 +0800

    1.解决速度配置5档时不会生效的问题 2.优化log，log可配置不输出 3.增加功能：在原神未启动时不进行对话识别，避免影响PC性能

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import time
 import random
 import ctypes, sys
+import threading
+import win32gui
+import win32con
 # 保护措施，避免失控
 pyautogui.FAILSAFE = True
 # 为所有的PyAutoGUI函数增加延迟。默认延迟时间是0.1秒。
@@ -26,11 +29,53 @@ timeFilter = 0.01
 SpdLevel = 2
 SpdLevelTable = [1,2,4,6,10]
 
+# log是否需打印:0:false,1:true
+logLevel = 0
+
+# 原神启动标志
+yuanshenRunning = False
+lock = threading.Lock()
+
+
+class timerThread(threading.Thread):
+    def run(self):
+        while(True):
+            isRunning()
+            time.sleep(12)
+
+#每15秒检测一次原神是否启动或已被最小化
+def isRunning():
+    global yuanshenRunning
+    #process = len(os.popen('''tasklist | findstr YuanShen.exe''').readlines())
+    #print(process)
+    window = win32gui.FindWindow(None, "原神")
+    minimized = True
+    if window:
+        tup = win32gui.GetWindowPlacement(window)
+        if tup[1] == win32con.SW_SHOWMINIMIZED:
+            minimized = True
+        else:
+            minimized = False
+            
+    lock.acquire()
+    if(False == minimized):
+        yuanshenRunning = True
+    else:
+        yuanshenRunning = False
+    lock.release()
+    if(False==yuanshenRunning):
+        logPrint("原神未启动")
+
+def logPrint(strlog):
+    global logLevel
+    if(0 != logLevel):
+        print(strlog)
+    
 def wait(x):
-    print("等待 "+str(x)+"s")
+    logPrint("等待 "+str(x)+"s")
     tep = int(x*10)
     for i in range(0, tep):
-        #print(i + 1)
+        #logPrint(i + 1)
         time.sleep(0.1)
 
 def getFilteredTime(x, deltaTime = 5, xFilter = 1.0):
@@ -42,22 +87,22 @@ def click():
         coords2 = pyautogui.locateOnScreen(path5, confidence=0.8)
         if(coords != None and coords2 != None):
             xy = pyautogui.center(coords)
-            print("已找到对话选项")
+            logPrint("已找到对话选项")
             wait(getFilteredTime(waitTime_Talk))  # 等waitTime秒把话说完
             pyautogui.moveTo(x=getFilteredTime(xy.x, 20, 100), y=getFilteredTime(xy.y, 2, 100), duration=getFilteredTime(0.2, 2))
             pyautogui.click()
-            print("已点击选项")
+            logPrint("已点击选项")
             return 1
         else:
             coords = pyautogui.locateOnScreen(path4, confidence=0.8)
             if(coords != None):
                 xy = pyautogui.center(coords)
-                print("已找到对话框")
+                logPrint("已找到对话框")
                 pyautogui.moveTo(x=getFilteredTime(xy.x+750, 50, 100), y=getFilteredTime(xy.y+400, 10, 100), duration=getFilteredTime(0.2, 2))
                 pyautogui.click()
-                print("已点击对话框，加速对话")
+                logPrint("已点击对话框，加速对话")
                 return 2
-            print("未找到对话")
+            logPrint("未找到对话")
             return 0
     except:
         print("程序出错了")
@@ -65,20 +110,25 @@ def click():
 def creatCfgFile():
     global coldInitFlag
     global SpdLevel
+    global logLevel
     if(os.path.exists("Genshin.cfg")):
         coldInitFlag = False
         file = open(".\\Genshin.cfg", "r")
         strLines = file.readlines()
         for strLine in strLines:
-            if(strLine.split("Speed=")[0].strip()=="" and int(strLine.strip().split("Speed=")[1]) in range(1,5)):
+            if(strLine.split("Speed=")[0].strip()=="" and int(strLine.strip().split("Speed=")[1]) in range(1,6)):
                 SpdLevel = int(strLine.split("Speed=")[1].strip())
-                break
+                continue
+            if(strLine.split("log=")[0].strip()=="" and int(strLine.strip().split("log=")[1]) in range(0,2)):
+                logLevel = int(strLine.split("log=")[1].strip())
+                continue
         file.close()
     else:
         coldInitFlag = True
         file = open(".\\Genshin.cfg", "w+")
         file.write(f"#可配置5种点击速度档，1最慢，5最快,在Speed=后加数字即可配置\n")
-        file.write("Speed=2")
+        file.write("Speed=2\n")
+        file.write("log=1\n")
         file.close()
     return coldInitFlag
 
@@ -113,10 +163,17 @@ def main():
     if(coldInitFlag):
         op = pyautogui.confirm(text=f'请将游戏分辨率调整为1600*900或以上，并打开剧情自动\n确认后将自动进行剧情对话点击\n本程序目录{os.getcwd()}下Genshin.cfg文件可配置点击速度\n默认速度等级:2',
                            title='原神剧情小助手', buttons=['OK', 'Cancel'])
-   
+    scanTimer = timerThread()
+    scanTimer.start()
+    print("助手已启动")
     if(op == 'OK'):
         while(True):
-            c = click()
+            c = 0xFF
+            #只有在原神启动时才进行对话检测
+            if(True == yuanshenRunning):
+                c = click();
+            else:
+                time.sleep(2)
             # 点到对话选项(1)或者对话框(2)之后x秒
             if(1 == c):
                 wait(getFilteredTime(waitTime_optionClick))
