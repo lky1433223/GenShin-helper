0319ac6d6b948ca169b0b7f2d44f1fade5606604
ShiinaRikka <50125419+lky1433223@users.noreply.github.com>
2023-04-08 00:00:52 +0800

    使用PEP 8 对代码进行了格式化（pycharm报错太烦了）

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import sys
 import threading
 import win32gui
 import win32con
-import win32api
 from pynput import keyboard
-from threading import Thread
+
 # 保护措施，避免失控
 pyautogui.FAILSAFE = True
 # 为所有的PyAutoGUI函数增加延迟。默认延迟时间是0.1秒。
@@ -44,16 +43,17 @@ lock = threading.Lock()
 enableFlag = False
 
 
-class timerThread(threading.Thread):
+class TimerThread(threading.Thread):
     def run(self):
-        while (True):
-            isRunning()
+        while True:
+            is_running()
             time.sleep(12)
+
 
 # 每15秒检测一次原神是否启动或已被最小化
 
 
-def isRunning():
+def is_running():
     global yuanshenRunning
     # process = len(os.popen('''tasklist | findstr YuanShen.exe''').readlines())
     # print(process)
@@ -67,80 +67,80 @@ def isRunning():
             minimized = False
 
     lock.acquire()
-    if (False == minimized):
+    if not minimized:
         yuanshenRunning = True
     else:
         yuanshenRunning = False
     lock.release()
-    if (False == yuanshenRunning):
-        logPrint("原神未启动")
+    if not yuanshenRunning:
+        log_print("原神未启动")
 
 
-def logPrint(strlog):
+def log_print(strlog):
     global logLevel
-    if (0 != logLevel):
+    if 0 != logLevel:
         print(strlog)
 
 
 def wait(x):
-    logPrint("等待 " + str(x) + "s")
+    log_print("等待 " + str(x) + "s")
     tep = int(x * 10)
     for i in range(0, tep):
         # logPrint(i + 1)
         time.sleep(0.1)
 
 
-def getFilteredTime(x, deltaTime=5, xFilter=1.0):
-    return x + timeFilter * random.randint(-deltaTime, deltaTime) * xFilter
+def get_filtered_time(x, delta_time=5, x_filter=1.0):
+    return x + timeFilter * random.randint(-delta_time, delta_time) * x_filter
 
 
 def click():
     try:
         coords = pyautogui.locateOnScreen(path3, confidence=0.8)
         coords2 = pyautogui.locateOnScreen(path5, confidence=0.8)
-        if (coords is not None and coords2 is not None):
+        if coords is not None and coords2 is not None:
             xy = pyautogui.center(coords)
-            logPrint("已找到对话选项")
-            wait(getFilteredTime(waitTime_Talk))  # 等waitTime秒把话说完
+            log_print("已找到对话选项")
+            wait(get_filtered_time(waitTime_Talk))  # 等waitTime秒把话说完
             pyautogui.moveTo(
-                x=getFilteredTime(
-                    xy.x, 20, 100), y=getFilteredTime(
-                    xy.y, 2, 100), duration=getFilteredTime(
+                x=get_filtered_time(
+                    xy.x, 20, 100), y=get_filtered_time(
+                    xy.y, 2, 100), duration=get_filtered_time(
                     0.2, 1))
             pyautogui.click()
-            logPrint("已点击选项")
+            log_print("已点击选项")
             return 1
         else:
             coords = pyautogui.locateOnScreen(path4, confidence=0.8)
-            if (coords is not None):
+            if coords is not None:
                 xy = pyautogui.center(coords)
-                logPrint("已找到对话框")
+                log_print("已找到对话框")
                 pyautogui.moveTo(
-                    x=getFilteredTime(
+                    x=get_filtered_time(
                         xy.x + 450,
                         50,
                         100),
-                    y=getFilteredTime(
+                    y=get_filtered_time(
                         xy.y + 300,
                         10,
                         100),
-                    duration=getFilteredTime(
+                    duration=get_filtered_time(
                         0.2,
                         1))
                 pyautogui.click()
-                logPrint("已点击对话框，加速对话")
+                log_print("已点击对话框，加速对话")
                 return 2
-            logPrint("未找到对话")
+            log_print("未找到对话")
             return 0
-    except BaseException:
-        print("程序出错了")
+    except BaseException as err:
+        print("程序出错了", err)
 
 
 def creatCfgFile():
     global coldInitFlag
     global SpdLevel
     global logLevel
-    if (os.path.exists("Genshin.cfg")):
+    if os.path.exists("Genshin.cfg"):
         coldInitFlag = False
         file = open(".\\Genshin.cfg", "r")
         strLines = file.readlines()
@@ -178,7 +178,8 @@ def iniCfg():
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
-    except BaseException:
+    except BaseException as err:
+        print(err)
         return False
 
 
@@ -199,83 +200,39 @@ def main():
     op = "OK"
     iniCfg()
     on_activate()
-    if (coldInitFlag):
+    if coldInitFlag:
         op = pyautogui.confirm(
             text=f'请将游戏分辨率调整为1600*900或以上，并打开剧情自动\n确认后将自动进行剧情对话点击\n本程序目录{os.getcwd()}下Genshin.cfg文件可配置点击速度\n默认速度等级:2',
             title='原神剧情小助手',
             buttons=[
                 'OK',
                 'Cancel'])
-    scanTimer = timerThread()
+    scanTimer = TimerThread()
     scanTimer.start()
 
-    if (op == 'OK'):
-        while (True):
+    if op == 'OK':
+        while True:
             c = 0xFF
             # 只有在原神启动且脚本激活时才进行对话检测
-            if (True == yuanshenRunning and enableFlag):
+            if yuanshenRunning and enableFlag:
                 c = click()
             else:
                 time.sleep(2)
             # 点到对话选项(1)或者对话框(2)之后x秒
-            if (1 == c):
-                wait(getFilteredTime(waitTime_optionClick))
-            elif (2 == c):
-                wait(getFilteredTime(waitTime_dialogClick))
+            if 1 == c:
+                wait(get_filtered_time(waitTime_optionClick))
+            elif 2 == c:
+                wait(get_filtered_time(waitTime_dialogClick))
             else:
-                wait(getFilteredTime(waitTime_noDialogClick))
+                wait(get_filtered_time(waitTime_noDialogClick))
     else:
         pyautogui.alert(text='感谢您的使用，再见', title='原神剧情小助手', button='OK')
-
-
-'''
-CTRL = False
-VK_5 = False
-VK_53 = False
-def keyListen():  # 键盘监听函数
-    def on_press(key):
-        global enableFlag
-      #  print(key)
-        global CTRL, VK_5, VK_53
-        if key == keyboard.Key.ctrl or key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-            CTRL = True
-        elif key == keyboard.KeyCode(char='5'):
-            VK_5 = True
-        elif key == keyboard.KeyCode(vk=53):
-            VK_53 = True
-
-        if (CTRL==True and VK_5==True) or (VK_53 == True):  # 检测到CTRL和5同时按下时，启动/关闭脚本
-            #CTRL = VK_5 = False5
-            enableFlag = not enableFlag
-            print(enableFlag, "success")
-
-    def on_release(key):
-       #print("onlistenRelease")
-        global CTRL, VK_5, VK_53
-        if key == keyboard.Key.ctrl or key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-            CTRL = False
-        elif key == keyboard.KeyCode(char='5'):
-            VK_5 = False
-        elif key == keyboard.KeyCode(vk=53):
-            VK_53 = False
-        #print(enableFlag)
-
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-
-class ListenThread(Thread):  # 按键监听线程　
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        keyListen()
-'''
 
 
 def on_activate():
     global enableFlag
     enableFlag = not enableFlag
-    if True == enableFlag:
+    if enableFlag:
         print('助手状态: 已开启, 可按 Ctrl+Shift+9 关闭')
     else:
         print('助手状态: 已关闭, 可按 Ctrl+Shift+9 开启')
@@ -291,8 +248,8 @@ if __name__ == '__main__':
     hotkey = keyboard.HotKey(
         keyboard.HotKey.parse('<ctrl>+<shift>+9'), on_activate)
     with keyboard.Listener(
-        on_press=for_canonical(hotkey.press),
-        on_release=for_canonical(hotkey.release)
+            on_press=for_canonical(hotkey.press),
+            on_release=for_canonical(hotkey.release)
     ) as l:
         {l.join()}
     mThread.join()
